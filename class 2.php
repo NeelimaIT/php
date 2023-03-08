f7e93657777cd40783a86870f3e8a6af99d39b47
NeelimaIT <127119061+NeelimaIT@users.noreply.github.com>
2023-03-08 20:32:50 +0600

    Add files via upload

--- class 2.php
+++ class 2.php
@@ -1,17 +1,33 @@
 
 //* If else Condition
 <?php
-$totalnumber= 750;
-$percentage= $totalnumber/1000*100;
-echo "The Percentage is:",$percentage;
-echo "<br>";
-if($percentage>=80){
-    echo"A+";
-}elseif($percentage>=60){
-    echo"A grade";
-}else if($percentage>=50){
-    echo"B grade";
-}else{
-    echo"you will be considered as a failure";
+function greaternum($p,$q,$r,$s){
+    if($p>$q && $p>$r && $p>$s){
+        echo "$p will be the greater one";
+    }
+    elseif($q>$p && $q>$r && $q>$s ){
+            echo "$q will be the greater one";
+        }
+         elseif($r>$p && $r>$q && $r>$s ){
+            echo "$r will be the greater one";
+        }
+        else{
+            echo" $s will be the greater one";
+        }
+    }
+    greaternum(100,220,190,490);
+?>
+
+//*switch case
+
+<?php
+$lotterynum=10;
+switch($lotterynum)
+{
+case 1:echo"You May won the lottery";
+break;
+case 2:echo" You missed it for one digit only";
+break;
+default:echo" Sorry,it was not your day";
 }
 ?>
